0d909dca8ff9283dbd22a37f4b34ce8cfea653ec
SandeepVaman <sandeepvbujire@gmail.com>
2018-06-19 17:29:07 +0530

    Added responsiveness to the site

--- resources/js/script.js
+++ resources/js/script.js
@@ -50,7 +50,14 @@ $(document).ready(function(){
     /* Mobile Navigation */
     $('.js--nav-icon').click(function(){
         var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon');
+
         nav.slideToggle(200);
+        if(icon.attr('name') === 'menu'){
+            icon.attr('name', 'close');
+        }else{
+            icon.attr('name', 'menu');
+        }
     });
     
 });
